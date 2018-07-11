ec37c44b533371f6b41f6d8885fd7a2c4506e91b
Rishabh Chakrabarti <rishabh.chakrabarti@protonmail.com>
2018-07-11 22:25:17 +0530

    Bootstrapped Project with Spring Boot

---
+++ src/main/java/org/open/spamcheck/spamcheck/SpamCheckApplication.java
@@ -0,0 +1,12 @@
+package org.open.spamcheck.spamcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpamCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpamCheckApplication.class, args);
+	}
+}
---
+++ src/test/java/org/open/spamcheck/spamcheck/SpamCheckApplicationTests.java
@@ -0,0 +1,16 @@
+package org.open.spamcheck.spamcheck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpamCheckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
